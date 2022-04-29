d13f166bdac4265d91d3a625794cd6f9ccbb2b58
tsangsiu <tsangsiu@gmail.com>
2022-04-29 08:11:47 +0000

    Re-do #2, Codewars

--- Part_2/Codewars/02.rb
+++ Part_2/Codewars/02.rb
@@ -1,61 +1,53 @@
 =begin
 
-Problem:
-- Input: array of integers
-- Output: integer, the number of pairs
-- Rules:
-  - For empty array and single-value array, return 0
-  - If there are more pair of a certain number, count each pair only one,
-    e.g., [0, 0, 0, 0] => 2
-- Questions:
-  - There may be more than 1 possible combination of pairs,
-    how to determine if they are a pair?
-    - Later when I fully understand more the question, this is not a question
+# Problem
+- Given an array of integers,
+  - Return the number of pairs in that array
+- If an array is empty or contain only one integer, return 0
+- If there are more pairs of a certain number, count each pair only once
+  - For example, [0, 0, 0, 0] -> 2
 
-Examples:
-[1, 2, 5, 6, 5, 2] => 2 (2 pairs of 2 and 5)
-[1, 2, 2, 20, 6, 20, 2, 6, 2] => 4 (4 pairs of [2, 20] or [6, 2]])
+# Examples
+p pairs([1, 2, 5, 6, 5, 2]) == 2
+p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+p pairs([0, 0, 0, 0, 0, 0, 0]) == 3 
+p pairs([1000, 1000]) == 1
+p pairs([]) == 0
+p pairs([54]) == 0
 
-Brainstorm (Ding!!):
-- Knowing that pairs must come in two
-- To find the number of pair
-  - get rid of one of the integers which appear odd number of times (how?)
-  - the result is the number of the remaining integers divided by 2
+# Data Structires
+- Input: Array
+- Intermediate: Hash
+- Output: Integer
 
-Data Structure:
-- Input: array
-- Intermediate: array to keep track of the numbers that are in pairs
-- Output: integer
-
-Algorithm:
-- given an array
-- initialise an array called `pairs` to store the integers that are in pairs
-- iterate through the given array
-  - if the number occurs odd numbers of times in the given array, say n times, and `pairs` does not contain that number
-    - add (n - 1) copies of that number to `pairs`
-  - if the number oddurs even numbers of time in the given array, say m times, and `pairs` does not contain that number
-    - add m copies of that number to `pairs`
-- find the number of elements in `pairs` divided by 2
-- return the result
+# Algorithm
+- Initialize `pair_count` to 0
+- Initialize an empty Hash called `number_count`, with a default value 0
+- Iterate through the Array,
+  - Use each number in the Array as a key
+  - Increment its value by 1
+- Iterate through the values of the Hash `number_count`
+  - Each value here is the count of each number
+  - Increment `pair_count` by the value / 2
+- Return `pair_count`
 
 =end
 
-# Code:
-
-def pairs(array)
-  pairs = []
-  array.each do |integer|
-    if !pairs.include?(integer)
-      count = array.count(integer)
-      if count.odd?
-        pairs = pairs + [integer] * (count - 1)
-      else
-        pairs = pairs + [integer] * count
-      end
-    end
+def pairs(arr)
+  pair_count = 0
+  number_count = Hash.new(0)
+  arr.each do |num|
+    number_count[num] += 1
   end
-  pairs.size / 2
+  number_count.each_value do |value|
+    pair_count += value / 2
+  end
+  pair_count
 end
 
-p pairs([1, 2, 5, 6, 5, 2])
-p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2])
+p pairs([1, 2, 5, 6, 5, 2]) == 2
+p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+p pairs([0, 0, 0, 0, 0, 0, 0]) == 3 
+p pairs([1000, 1000]) == 1
+p pairs([]) == 0
+p pairs([54]) == 0
