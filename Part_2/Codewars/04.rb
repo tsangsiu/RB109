b80c56efc1304d6b793428f89bc59bfe46e785fb
tsangsiu <tsangsiu@gmail.com>
2022-05-02 10:27:18 +0800

    Re-do #4, Codewars

--- Part_2/Codewars/04.rb
+++ Part_2/Codewars/04.rb
@@ -1,72 +1,49 @@
 =begin
 
-Problem:
-- Input: array of words
-- Output: array of numbers
-- Rules:
-  - In the alphabet, a and b are in positions 1 and 2
-  - Conside the word 'adode', a and b are also in positions 1 and 2
-  - given an array of words, return an array of the number of letters that
-    occupy their positions in the alphabet for each word
-- Questions:
-  - case-sensitive?
-    - from the examples, 'a' and 'A' are treated as 'A'
-  - will there be spaces in the given words?
-    - No
+# Problem
+- Given an Array of Strings,
+  - Return the number of characters of each String that are in the correct alphabet position
+- Ignoring Cases
 
-- Examples:
-p solve(["abode", "ABc", "xyzD"]) == [4, 3, 1]
-p solve(["abide", "ABc", "xyz"]) == [4, 3, 0]
-p solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]) == [6, 5, 7]
-p solve(["encode", "abc", "xyzD", "ABmD"]) == [1, 3, 1, 3]
+# Examples
+p solve(["abode","ABc","xyzD"]) == [4, 3, 1]
+p solve(["abide","ABc","xyz"]) == [4, 3, 0]
+p solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"])== [6, 5, 7]
+p solve(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3]
 
-- Data structures:
-  - input: an array of words
-  - intermediate: the same array
-  - output: an array of integer of counts
-
-- Algorithm:
-  - conside only one word:
-    - create a constant `LETTERS`, which is an array of alphabets
-    - initialise a variable `count` to count
-    - given a single word
-    - split the word into characters
-    - iterate through the characters
-      - if the position of that characters is the same as the position of the corresponding alphabet
-        - increment one to count
-    - return count
-
-  - for the given array:
-    - initialise an array `counts` to store the counts
-    - iterate through each word
-      - apply the above method to get the count
-      - append it to `counts`
-    - return `counts`
+# Algorithm
+- A method to determine the number of a single String, called `solve_each`:
+  - Initialize an Array constant called `ALPHABETS`, where each element is an alphabet (lowercase) in alphabetical order
+  - Initialize `count` to `0`
+  - Iterate through each character in the String,
+    - If the character (considering lowercase only) at the current position is the same as that in `alphabets` at the same position,
+      - Increment `count` by `1`
+  - Return `count`
+- A method to determine the number of each String in an Array, called `solve`:
+  - Iterate through the Array,
+    - For each element, apply the above method to find the number
+    - Use that number to transform the String element
+  - Return the transformed Array
 
 =end
 
-# Code:
-
-LETTERS = ('a'..'z').to_a
-
-def count_alphabet_symmetry(word)
+# Code
+ALPHABETS = ('a'..'z').to_a
+def solve_each(str)
   count = 0
-  characters = word.split('')
-  characters.each_with_index do |character, index|
-    count += 1 if character.downcase == LETTERS[index]
+  str.each_char.with_index do |char, index|
+    count += 1 if char.downcase == ALPHABETS[index]
   end
   count
 end
 
-def solve(array_of_words)
-  counts = []
-  array_of_words.each do |word|
-    counts << count_alphabet_symmetry(word)
+def solve(arr)
+  arr.map do |str|
+    solve_each(str)
   end
-  counts
 end
 
-p solve(["abode", "ABc", "xyzD"]) == [4, 3, 1]
-p solve(["abide", "ABc", "xyz"]) == [4, 3, 0]
-p solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]) == [6, 5, 7]
-p solve(["encode", "abc", "xyzD", "ABmD"]) == [1, 3, 1, 3]
+p solve(["abode","ABc","xyzD"]) == [4, 3, 1]
+p solve(["abide","ABc","xyz"]) == [4, 3, 0]
+p solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"])== [6, 5, 7]
+p solve(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3]
